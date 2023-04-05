d42251dbf88329c54a9e9370cd018172c90450f8
i_rk_jain <svsravikiran.9@gmail.com>
2023-04-05 19:06:52 +0530

    Movable and Destructable

---
+++ Assets/Scripts/Road/Movable.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movable : MonoBehaviour {
+
+    protected float dOnRoad;
+    protected Vector2 pAtBirth;
+
+    protected void Awake() {
+        dOnRoad = Road.GetDistanceTravelled();
+        pAtBirth = transform.position;
+    }
+
+    protected void Update() {
+        float dByRoad = Road.GetDistanceTravelled() - dOnRoad;
+
+        Vector2 p = pAtBirth - new Vector2(dByRoad, 0);
+
+        transform.position = p;
+    }
+
+}
